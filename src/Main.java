4f24ffed6b25e28123ad0f1f8fdc78e56a9b9d33
Seryozha Sargsyan <9775927688s@gmail.com>
2022-11-11 11:08:00 +0300

    add gitignore

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 18000;
+        int valueBonus = 20;
+
+        int mileBonus = ticketPrice / valueBonus * 1;
+
+        System.out.println(mileBonus + " Мили");
+    }
+}
+
+
